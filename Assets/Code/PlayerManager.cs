a4db9eaeb6b267d34f3535b868c55eb4e7d721ea
Alex Otjens <ajc.otjens@student.avans.nl>
2025-03-18 10:28:08 +0100

    Added player movement

---
+++ Assets/Code/PlayerManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float speed = 5f;
+    public static PlayerManager Singleton;
+
+    private SpriteRenderer playerSprite;
+
+    private void Start()
+    {
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        // Beweging van de speler
+        if (Input.GetKey(KeyCode.A)) moveDirection.x -= 1;
+        if (Input.GetKey(KeyCode.D)) moveDirection.x += 1;
+
+        if (moveDirection.magnitude > 1) moveDirection.Normalize();
+        transform.position += moveDirection * speed * Time.deltaTime;
+    }
+}
