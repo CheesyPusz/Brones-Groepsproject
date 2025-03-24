c051228a35344e0790c173158c5407da324344de
FB99FIRE <felix99@hotmail.nl>
2025-03-24 09:30:40 +0100

    fix login

--- Assets/Code/LoginManager.cs
+++ Assets/Code/LoginManager.cs
@@ -10,6 +10,7 @@ public class LoginManager : MonoBehaviour
     [SerializeField] private TMP_InputField passwordField;
     
     private string difficulty = "not selected";
+    private string selectedRoute = "not selected";
     void Start()
     {
         
@@ -47,6 +48,37 @@ public class LoginManager : MonoBehaviour
         }
     }
 
+    public void SelectRouteA(bool toggleValue)
+    {
+        if (toggleValue == true)
+        {
+            Debug.Log("selected route A: gips");
+            selectedRoute = "A";
+            Debug.Log(selectedRoute);
+        }
+        else
+        {
+            selectedRoute = "not selected";
+            Debug.Log("desected route A");
+        }
+    }
+
+    public void SelectRouteB(bool toggleValue)
+    {
+        if (toggleValue == true)
+        {
+            Debug.Log("selected route B: operatie");
+            selectedRoute = "B";
+            Debug.Log(selectedRoute);
+        }
+        else
+        {
+            selectedRoute = "not selected";
+            Debug.Log("deselected route B");
+        }
+    }
+
+
     public async void Login()
     {
         string username = emailField.text;
@@ -56,18 +88,36 @@ public class LoginManager : MonoBehaviour
             Debug.LogError("Username or password is empty");
             return;
         }
-        if (difficulty == "not selected") 
-        {
-            Debug.LogError("Difficulty is not selected");
-            return;
-        }
         Debug.Log($"Logging in with username: {username} and password: {password} with difficulty: {difficulty}");
-        // Call the login API
-        // await LoginAPI.Login(username, password);
-        // Load the next scene
         SceneManager.LoadScene("SampleScene");
         await Task.Delay(1000); // added this so i can have an asyn Login(), which is needed for awaiting the webrequest. afterwards delete this await Task.Delay(1000)
     }
 
+    public async void Register()
+    {
+        string username = emailField.text;
+        string password = passwordField.text;
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            Debug.LogError("Username or password is empty");
+            return;
+        }
+        if (difficulty == "not selected" || selectedRoute == "not selected")
+        {
+            Debug.Log("Difficulty is not selected");
+            return;
+        }
+        if (selectedRoute == "not selected")
+        {
+            Debug.Log("Route is not selected");
+            return;
+        }
+        Debug.Log($"Selected route: {selectedRoute}");
+
+        Debug.Log($"Registering with username: {username}, password: {password} with difficulty: {difficulty}");
+
+        SceneManager.LoadScene("SampleScene");
+        await Task.Delay(1000); // added this so i can have an asyn Register(), which is needed for awaiting the webrequest. afterwards delete this await Task.Delay(1000)
+    }
 
 }
