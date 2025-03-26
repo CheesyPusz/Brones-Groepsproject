98669e3e98da9fabf387150ddcda547ed2ad1a4a
Bramvdk1 <bvdkerkhove@gmail.com>
2025-03-26 15:30:59 +0100

    commit

--- Assets/Code/Inloggen en gegevens/GegevensManager.cs
+++ Assets/Code/Inloggen en gegevens/GegevensManager.cs
@@ -1,12 +1,26 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //Deze klasse uiteindelijk uitbreiden voor de info als dat lukt
 
 public class GegevensManager : MonoBehaviour
 {
+    
+    public Toggle toggleA;
+    public Toggle toggleB;
+    public TMP_InputField NaamKind;
+    public TMP_InputField NaamArts;
+    void Start()
+    {
+        // Zorgt ervoor dat beide toggles uit staan bij start
+        toggleA.isOn = false;
+        toggleB.isOn = false;
+    }
     public void Continue()
     {
         SceneManager.LoadScene("IntroductieScherm");
     }
+
 }
